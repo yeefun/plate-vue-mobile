1693689d217108962fffaf68b4e585c000a4c354
Hsin-chan Chien <hcchien@MM-CP01050002.local>
2019-05-01 01:24:08 +0800

    modify the log

--- api/middle/redisHandler.js
+++ api/middle/redisHandler.js
@@ -182,8 +182,9 @@ const fetchFromRedis = (req, res, next) => {
 const fetchFromRedisForAPI = (req, res, next) => {
   debug('Trying to fetching data from redis...', req.url)
   redisFetching(req.url, ({ error, data }) => {
+    let start = Date.now()
     if (!error && data) {
-      let start = Date.now()
+	  let timePeriod = Date.now() - start
 	  if (timePeriod > 500) {
         console.log('Fetch data from Redis.', `${timePeriod}ms`, req.url)
       }
